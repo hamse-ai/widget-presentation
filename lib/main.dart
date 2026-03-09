38abc8469392cdd457a06db9519a6e98b7b03925
hamse-ai <h.jama@alustudent.com>
2026-03-09 13:36:46 +0300

    Initial commit: Flutter RotationTransition widget demo

---
+++ lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: AnimationDemoScreen(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class AnimationDemoScreen extends StatefulWidget {
+  const AnimationDemoScreen({super.key});
+
+  @override
+  State<AnimationDemoScreen> createState() => _AnimationDemoScreenState();
+}
+
+class _AnimationDemoScreenState extends State<AnimationDemoScreen> with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 1),
+    )..repeat(); // Makes it rotate infinitely
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose(); // Always clean up!
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: RotationTransition(
+          turns: _controller,
+          child: Container(width: 10, height: 150, color: Colors.deepPurple),
+        ),
+      ),
+    );
+  }
+}
